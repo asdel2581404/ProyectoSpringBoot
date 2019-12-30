0b68c8861c5d75e7278380c98e751bd52c050083
Evin Mosquera <evin.mosquera026@outlook.es>
2019-12-30 10:12:35 -0500

    Se agrega generated para manejo de autoincrementable en Springboot

--- src/main/java/com/banco/Entity/InformacionDomicilio.java
+++ src/main/java/com/banco/Entity/InformacionDomicilio.java
@@ -2,6 +2,8 @@ package com.banco.Entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,7 +12,8 @@ import javax.persistence.Table;
 
 public class InformacionDomicilio {
 	
-	@Id
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY) 
 	@Column(name="idinfodomicilio")
 	private long idinfodomicilio;
 	
